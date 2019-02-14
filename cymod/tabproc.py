8ec07b5f8ec86380908a99579c54174291de13e0
Andrew Lane <ajlane50@gmail.com>
2019-02-14 15:08:06 +0000

    Successfully producing queries for several condition columns

--- cymod/tabproc.py
+++ cymod/tabproc.py
@@ -6,9 +6,12 @@ cymod.tabproc
 This module contains classes involved in loading Cypher queries from a tabular
 data source.
 """
+import six
+
 import pandas as pd
 
 from cymod.params import validate_cypher_params
+from cymod.cybase import CypherQuery, CypherQuerySource
 
 class TransTableProcessor(object):
     """Processes a :obj:`pandas.DataFrame` and produces Cypher queries.
@@ -35,7 +38,69 @@ class TransTableProcessor(object):
         self.start_state_col = start_state_col
         self.end_state_col = end_state_col
         self.labels = labels
-        self.global_params = None
+        self.global_params = global_params
+
+
+    def _row_to_query_statement_string(self, row):
+        """Build the string specifying the cypher query for a single row."""
+        start_node = 'MERGE (start:{state_lab} {{code:"{start_state}"}})'\
+            .format(state_lab="State", start_state=row[self.start_state_col])
+
+        end_node = 'MERGE (end:{state_lab} {{code:"{end_state}"}})'\
+            .format(state_lab="State", end_state=row[self.end_state_col])
+
+        transition \
+            = 'MERGE (start)<-[:SOURCE]-(trans:{trans_lab})-[:TARGET]->(end)'\
+                . format(trans_lab="Transition")
+
+        def _conditions_str(row, start_state_col, end_state_col):
+            """Build the string used to express transition conditions.
+            
+            This is the part in curly braces specifying the Condition node's
+            properties.
+            """
+            row = row.drop([start_state_col, end_state_col])
+            count = 0                                
+            s = ""                                   
+            for i, val in row.iteritems():   
+                s += i + ":"                         
+                if isinstance(val, six.string_types):
+                    s += '"' + val + '"'
+                elif isinstance(val, bool):
+                    s += str(val).lower()
+                else:
+                    s += str(val)    
+                if count < len(row) - 1:
+                    s += ", " 
+                count += 1
+
+            return "{" + s + "}"
+
+        condition = 'MERGE (cond:{cond_lab} {cond_str})-[:CAUSES]->(trans)'\
+                .format(cond_lab="Condition", 
+                    cond_str=_conditions_str(row, self.start_state_col, 
+                        self.end_state_col))
+
+        return start_node + " " + end_node + " " + transition + " "\
+            + condition + ";"
+
+    def _row_to_cypher_query(self, row_index, row):
+        statement = self._row_to_query_statement_string(row)
+        source = CypherQuerySource(self.df, "tabular", row_index)
+        return CypherQuery(statement, params=None, source=source)
+
+    def iterqueries(self):
+        for i, row in self.df.iterrows():
+            yield self._row_to_cypher_query(i, row)
+
+
+    
+
+        
+
+        
+
+
 
     
 
--- tests/test_tabproc.py
+++ tests/test_tabproc.py
@@ -19,6 +19,14 @@ class TransTableProcessorTestCase(unittest.TestCase):
             "cond": ["low", "high"]
         })
 
+        self.demo_explicit_table_more_conds = pd.DataFrame({
+            "start": ["state1", "state2"], 
+            "end": ["state2", "state3"], 
+            "cond1": ["low", "high"],
+            "cond2": [2, 3],
+            "cond3": [True, False]
+        })
+
     def tearDown(self):
         del self.demo_explicit_table    
 
@@ -40,9 +48,39 @@ class TransTableProcessorTestCase(unittest.TestCase):
             + 'MERGE (cond:Condition {cond:"high"})-[:CAUSES]->(trans);'
         )
 
-        self.assertEqual(query_iter.next(), query1)
+        #this_query = query_iter.next()
+        self.assertEqual(query_iter.next().statement, query1.statement)
 
-        self.assertEqual(query_iter.next(), query2)
+        #this_query = query_iter.next()
+        self.assertEqual(query_iter.next().statement, query2.statement)
+        
+        self.assertRaises(StopIteration, query_iter.next)
+
+    def test_explicit_codes_queries_multiple_conds_correct(self):
+        """Correct queries with multiple conditions."""
+        ttp = TransTableProcessor(self.demo_explicit_table_more_conds,
+            "start", "end")
+        query_iter = ttp.iterqueries()
+
+        query1 = CypherQuery('MERGE (start:State {code:"state1"}) '
+            + 'MERGE (end:State {code:"state2"}) '
+            + 'MERGE (start)<-[:SOURCE]-(trans:Transition)-[:TARGET]->(end) '
+            + 'MERGE (cond:Condition {cond1:"low", cond2:2, cond3:true})'
+            + '-[:CAUSES]->(trans);'
+            )
+
+        query2 = CypherQuery('MERGE (start:State {code:"state2"}) '
+            + 'MERGE (end:State {code:"state3"}) '
+            + 'MERGE (start)<-[:SOURCE]-(trans:Transition)-[:TARGET]->(end) '
+            + 'MERGE (cond:Condition {cond1:"high", cond2:3, cond3:false})'
+            + '-[:CAUSES]->(trans);'
+        )
+
+        #this_query = query_iter.next()
+        self.assertEqual(query_iter.next().statement, query1.statement)
+
+        #this_query = query_iter.next()
+        self.assertEqual(query_iter.next().statement, query2.statement)
         
         self.assertRaises(StopIteration, query_iter.next)
 
